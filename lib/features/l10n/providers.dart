e0e6850721b7db662321baadc50089503e312d55
using-system <mnicolescu@gmail.com>
2026-04-18 12:04:59 +0200

    feat(l10n): add locale preference and effective locale providers

---
+++ lib/features/l10n/providers.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'data/locale_preference_storage.dart';
+import 'domain/locale_preference.dart';
+
+final sharedPreferencesProvider = FutureProvider<SharedPreferences>((ref) {
+  return SharedPreferences.getInstance();
+});
+
+final localePreferenceStorageProvider =
+    FutureProvider<LocalePreferenceStorage>((ref) async {
+  final prefs = await ref.watch(sharedPreferencesProvider.future);
+  return LocalePreferenceStorage(prefs);
+});
+
+class LocalePreferenceNotifier extends AsyncNotifier<LocalePreference> {
+  @override
+  Future<LocalePreference> build() async {
+    final storage = await ref.watch(localePreferenceStorageProvider.future);
+    return storage.read();
+  }
+
+  Future<void> setPreference(LocalePreference preference) async {
+    final storage = await ref.read(localePreferenceStorageProvider.future);
+    state = const AsyncValue.loading();
+    try {
+      await storage.write(preference);
+      state = AsyncValue.data(preference);
+    } catch (error, stack) {
+      state = AsyncValue.error(error, stack);
+    }
+  }
+}
+
+final localePreferenceProvider =
+    AsyncNotifierProvider<LocalePreferenceNotifier, LocalePreference>(
+  LocalePreferenceNotifier.new,
+);
+
+final effectiveLocaleProvider = Provider<Locale?>((ref) {
+  final preference = ref.watch(localePreferenceProvider);
+  return preference.whenOrNull(
+    data: (value) => switch (value) {
+      SystemLocalePreference() => null,
+      ForcedLocalePreference(:final locale) => locale,
+    },
+  );
+});

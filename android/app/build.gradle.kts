2183cb69545b265806cd94bb2eb9adb76537fa7d
using-system <mnicolescu@gmail.com>
2026-04-18 09:29:58 +0200

    chore(app): set display name to Cookmate and bundle id to com.cookmate.app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.cookmate.cookmate"
+    namespace = "com.cookmate.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.cookmate.cookmate"
+        applicationId = "com.cookmate.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
